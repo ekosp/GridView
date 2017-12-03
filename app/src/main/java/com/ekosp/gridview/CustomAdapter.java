4e8cd182275d0a3fc33eb36fa842bd9b9e93260a
ekosp <ekosetyopurnomo@gmail.com>
2017-12-03 21:39:49 +0700

    feat: Add Adapter, Drawable and Detail Activity
    Drawable image as resource image to diplayed on view (grid)
    MainActivity : as main display, present gridView
    DetailActivity : display single (image) view from grid
    Adapter : used to mapping drawable to each gridView column / row

---
+++ app/src/main/java/com/ekosp/gridview/CustomAdapter.java
@@ -0,0 +1,50 @@
+package com.ekosp.gridview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+/**
+ * Created by Eko Setyo Purnomo on 03-Dec-17.
+ * Find me on https://ekosp.com
+ * Or email me directly to ekosetyopurnomo@gmail.com
+ */
+
+public class CustomAdapter extends BaseAdapter {
+    Context context;
+    int logos[];
+    LayoutInflater inflter;
+
+    public CustomAdapter(Context applicationContext, int[] logos) {
+        this.context = applicationContext;
+        this.logos = logos;
+        inflter = (LayoutInflater.from(applicationContext));
+    }
+
+    @Override
+    public int getCount() {
+        return logos.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        view = inflter.inflate(R.layout.activity_gridview, null); // inflate the layout
+        ImageView icon = (ImageView) view.findViewById(R.id.icon); // get the reference of ImageView
+        icon.setImageResource(logos[i]); // set logo images
+        return view;
+    }
+}
+
